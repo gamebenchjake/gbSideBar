3bc44e859b243b98a4c69e3ddd3c4e830de11dec
jdaynes <jdaynes@bitbucket.org>
2014-09-29 17:35:27 +0100

    fixed bug

--- dist/gbsidebar.js
+++ dist/gbsidebar.js
@@ -1,7 +1,7 @@
 jQuery.fn.gbDrawer = function(open) {
 	var drawer = jQuery(this);
 	var content = jQuery(this).siblings();
-	drawer.css('height', $(document).height() - drawer.offsetTop);
+	drawer.css('height', $(document).height() - drawer.offsetTop - 0);
 	if (typeof open != 'undefined') {
 	} else {
 		var open = false;
