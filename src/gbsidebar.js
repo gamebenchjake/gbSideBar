0358ed0577e86d6db9af12b8d0277599fafdc680
jdaynes <jdaynes@bitbucket.org>
2014-10-01 12:05:10 +0100

    bug fix

--- src/gbsidebar.js
+++ src/gbsidebar.js
@@ -148,7 +148,7 @@ function gbOpenSlide(drawer, content) {
 	}
 	if ($(window).width() > 600) {
 		content.attr('style', "margin: 0px 0px 0px 325px");
-		})
+		}
 	}
 	drawer.removeClass('gbclosed');
 	drawer.addClass('gbopen');
@@ -168,7 +168,7 @@ function gbCloseSlide(drawer, content) {
 	}
 	if ($(window).width() > 600) {
 		content.attr('style', "margin: 0px;left: -325px;position: absolute;width: calc(100% + 325px);");
-		})
+		}
 	}
 	drawer.removeClass('gbopen');
 	drawer.addClass('gbclosed');
