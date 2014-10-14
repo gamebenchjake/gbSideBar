2374fa73c79fafde0eacce731b6ea8ad52ca0b80
jdaynes <jdaynes@bitbucket.org>
2014-10-14 18:44:53 +0100

    css fix

--- src/gbsidebar.js
+++ src/gbsidebar.js
@@ -172,7 +172,7 @@ function gbOpenSlide(drawer, content) {
 }
 
 function gbCloseSlide(drawer, content) {
-	$('.gbcontent').css('height', '100%');
+	$('.gbcontent').css('max-height', '100%');
 	$('.gbcontent').css('float', 'right');
 	$('.gbcontent').css('position', 'inherit');
 	var ph = $('.gbcontent').parent().height();
